8771db16cbdbae44bcf734957378c8affde2af2e
TKTheTechie <thomas.kunnumpurath@solace.com>
2022-02-01 11:57:25 -0500

    Adding vpn connection

--- src/main/java/com/solace/redeliveryservice/impl/BasicSolaceMessagingService.java
+++ src/main/java/com/solace/redeliveryservice/impl/BasicSolaceMessagingService.java
@@ -27,6 +27,9 @@ public class BasicSolaceMessagingService extends ISolaceMessagingService {
     @Value("${solace.password}")
     private String password;
 
+    @Value("${solace.vpn}")
+    private String vpn;
+
 
     /**
      * Reads in the properties and initializes the MessagingService
@@ -37,6 +40,7 @@ public class BasicSolaceMessagingService extends ISolaceMessagingService {
         serviceConfiguration.setProperty(SolaceProperties.TransportLayerProperties.HOST,host);
         serviceConfiguration.setProperty(SolaceProperties.AuthenticationProperties.SCHEME_BASIC_USER_NAME, user);
         serviceConfiguration.setProperty(SolaceProperties.AuthenticationProperties.SCHEME_BASIC_PASSWORD, password);
+        serviceConfiguration.setProperty(SolaceProperties.ServiceProperties.VPN_NAME,  vpn);
 
         this.solaceMessagingService = MessagingService.builder(ConfigurationProfile.V1).fromProperties(serviceConfiguration).build().connect();
         super.init();
